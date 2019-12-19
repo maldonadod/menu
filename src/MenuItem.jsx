c965d7c84fbbb064f2c99c31251b12e8aea3d171
Daniel Maldonado <daniel.maldonado@bluealba.com>
2019-12-19 13:31:12 -0300

    added menu item css

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,8 +1,9 @@
 import React from "react"
+import "./MenuItem.css"
 
 function MenuItem(props) {
   return (
-    <div onClick={props.onClick}>
+    <div className="menu-item" onClick={props.onClick}>
       {props.title}
     </div>
   )
